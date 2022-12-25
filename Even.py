2fe66d4277aa7def952602e902e6f9a7e06f568a
Ssk161 <110150079+Ssk161@users.noreply.github.com>
2022-12-25 11:13:02 +0530

    swap values without using 3rd variable

--- Even.py
+++ Even.py
@@ -1,5 +1,17 @@
+#Chceking even or odd number
 n=int(input())
 if(n%2==0):
     print("even")
 else:
     print("odd")
+
+#swap values without using 3rd variable
+a,b=map(int,input().split())
+print("before sorting",a,b)
+a=a+b
+b=a-b
+a=a-b
+print("after sorting",a,b)
+
+
+
